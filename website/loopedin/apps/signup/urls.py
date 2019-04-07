29e74434ce12fe341a48ee92526d28bdfb3470e8
= <shreepa.parthaje@gmail.com>
2019-04-06 22:41:32 -0400

    django stuff

--- website/loopedin/apps/signup/urls.py
+++ website/loopedin/apps/signup/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path(r'', views.signup, name="signup"),
     path(r'info', views.index, name="index"),
     path(r'congress', views.congress, name="congress"),
+    path(r'topics', views.topics, name="topics"),
+    path(r'confirm', views.confirm, name="confirm"),
     path(r'zip-handler', views.zip_handler, name="zip_handler"),
-    path(r'user-handler', views.user_handler, name="user_handler")
+    path(r'user-handler', views.user_handler, name="user_handler"),
+    path(r'topics-handler', views.topics_handler, name="topics_handler"),
+    path(r'push-handler', views.push_handler, name="push_handler")
 ]
