8da86917e191e1ec48fb3e7090a1ec5d9ede66cb
= <shreepa.parthaje@gmail.com>
2019-04-06 20:10:10 -0400

    django stuff

--- website/loopedin/apps/signup/urls.py
+++ website/loopedin/apps/signup/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name="testapp"
+app_name = "testapp"
 urlpatterns = [
-    path(r'', views.index, name="index"),
-    path(r'zip-handler', views.zip_handler, name="zip_handler")
+    path(r'', views.signup, name="signup"),
+    path(r'info', views.index, name="index"),
+    path(r'congress', views.congress, name="congress"),
+    path(r'zip-handler', views.zip_handler, name="zip_handler"),
+    path(r'user-handler', views.user_handler, name="user_handler")
 ]
--- website/loopedin/apps/signup/views.py
+++ website/loopedin/apps/signup/views.py
@@ -1,13 +1,84 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, reverse
 
-zipcode = 0
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+import requests as pyreq
+
+import json
 
 def index(request):
-    print(zipcode)
     return render(request, 'signup/index.html')
 
 
+def signup(request):
+    return render(request, 'signup/signup.html')
+
+
+def congress(request):
+    response = request.session.get("response")
+
+    context = {
+        "REP_NAME": response["Representatives"][0]["name"],
+        "REP_PARTY": response["Representatives"][0]["position"],
+        "REP_PHOTO": response["Representatives"][0]["photoURL"],
+        "REP_WEBSITE": response["Representatives"][0]["web"],
+        "SEN_1_NAME": response["Senators"][0]["name"],
+        "SEN_1_PARTY": response["Senators"][0]["position"],
+        "SEN_1_PHOTO": response["Senators"][0]["photoURL"],
+        "SEN_1_WEBSITE": response["Senators"][0]["web"],
+        "SEN_2_NAME": response["Senators"][1]["name"],
+        "SEN_2_PARTY": response["Senators"][1]["position"],
+        "SEN_2_PHOTO": response["Senators"][1]["photoURL"],
+        "SEN_2_WEBSITE": response["Senators"][1]["web"],
+    }
+
+    if context["REP_PHOTO"] is "":
+        context["REP_PHOTO"] = "https://www.r-users.com/wp-content/plugins/all-in-" \
+                               "one-seo-pack/images/default-user-image.png"
+
+    if context["SEN_1_PHOTO"] is "":
+        context["SEN_1_PHOTO"] = "https://www.r-users.com/wp-content/plugins/all-" \
+                                 "in-one-seo-pack/images/default-user-image.png"
+
+    if context["SEN_2_PHOTO"] is "":
+        context["SEN_2_PHOTO"] = "https://www.r-users.com/wp-content/plugins/all-in-" \
+                                 "one-seo-pack/images/default-user-image.png"
+
+    return render(request, 'signup/congress.html', context=context)
+
+
 def zip_handler(request):
-    global zipcode
-    zipcode = int(request.POST["zipcode"])
-    return HttpResponse("h")
+    state = request.POST["state"]
+    zipcode = request.POST["zipcode"]
+    response = pyreq.post('https://loopedin-backend.herokuapp.com/reps',
+                                  data={"state": state, "zip": zipcode})
+
+    request.session['state'] = state
+    request.session['zipcode'] = zipcode
+
+    f = open('file.txt', 'w')
+    f.write(response.text)
+    f.close()
+
+    data = json.load(open('file.txt', 'r'))
+
+    request.session['response'] = data
+
+    return HttpResponseRedirect("/congress")
+
+
+def user_handler(request):
+    username, password = request.POST["username"], request.POST["password"]
+    email = request.POST["username"] + "@gmail.com"
+    new_user = User.objects.create_user(username=username, password=password, email=email)
+    new_user.save()
+    login(request, new_user)
+    return HttpResponseRedirect("/info")
+
+
+"""
+response = requests.post('https://loopedin-backend.herokuapp.com/reps',data={"state":[state],"zip":[zip]})
+response = response.content
+
+"""
