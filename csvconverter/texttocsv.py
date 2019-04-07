170bccceb1a30e75319c993408a9a051f7af386b
Saketh Kura <sakethkura@Sakeths-MacBook-Pro.local>
2019-04-06 23:54:49 -0400

    txt to csv converter

--- csvconverter/texttocsv.py
+++ csvconverter/texttocsv.py
@@ -1,16 +1,50 @@
 import csv
 import os.path
 
-def main():
+#os.path.join(
+directory = "/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/na/business"
+#summarydirectory = "/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/sum/business"
+x=01;
+mycsv = csv.writer(open('businessoutput.csv', 'w'))
+mycsv.writerow(['Header', 'Original', 'Summary'])
+for filename in os.listdir(directory):
+	if x  >= 511:
+		break
+	if x < 10:
+		text_file = open("/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/na/business/00" + str(x) + ".txt", mode='r')	
+	elif x<100:
+		text_file = open("/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/na/business/0" + str(x) + ".txt", mode='r')
+	else:
+		text_file = open("/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/na/business/" + str(x) + ".txt", mode='r')
 	
-  	
-	file1=open("001.txt", "r")
-	In_text = csv.reader(file1, delimiter = '. ')
-	file3 = out_csv.writerow("Header", "Original Text", "Summary") 
-	mycsv = csv.writer(open('OutPut.csv', 'w'))
+	lines = text_file.readlines()
+	#print(lines);	
+	text_file.close() 
 
-	 
-	file1.close()
-	file2.close()
+
+	head = lines[0];
+	ori = ""
+	for line in lines:
+		ori +=line;
+	if x < 10:	
+		text_file2 = open("/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/sum/business/00" + str(x) + ".txt", mode='r')
+	elif x < 100:
+		text_file2 = open("/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/sum/business/0" + str(x) + ".txt", mode='r')
+	else:
+			text_file2 = open("/Users/sakethkura/Documents/HackTJ/LoopedIn/csvconverter/bbc/sum/business/" + str(x) + ".txt", mode='r')		
+	lines2 = text_file2.readlines()
+	summ = ""
+	for line2 in lines2:
+		summ += line2
+	mycsv.writerow([head, ori, summ])
+	x = x+1
+		
+
+	
+
+
+
+
+
 
 
